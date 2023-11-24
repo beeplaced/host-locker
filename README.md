a5cf4efe093f13dcbebcf902c141c56de80e72d9
beeplaced <you@example.com>
2023-11-24 12:24:50 +0100

    1.0.0 jsdoc types and readme

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # host-locker
-Indicating control and security for specific hosts.
+
+HostLocker is a Node.js middleware class designed for rate-limiting and controlling access based on the number of requests within a specified time window for specific hosts.
+
+## Table of Contents
+<!-- no toc -->
+  - [About](#about)
+  - [Installation and usage](#installation-and-usage)
+  - [Support](#support)
+
+
+## About
+
+The library checks if a given hostname is allowed to make a request based on the configured rate-limiting parameters.
+
+## Installation and usage
+
+  * Install the host-locker module using npm:
+
+```js
+$ npm install host-locker
+```
+
+  * Usage
+```js
+const HostLocker = require('host-locker');
+
+// Initialize HostLocker with configuration
+const hostLimiter = new HostLocker({
+  maxCallThreshold: 5, // Maximum allowed calls within the specified time window (default: 5)
+  secondsThreshold: 5, // Time window in seconds (default: 5)
+  allowedHosts: ['example.com', 'api.example.com']  // Allowed hosts (default: [])
+});
+
+// Check if a host is allowed to make a request
+const isAllowed = hostLimiter.check('example.com');
+
+if (isAllowed) {
+  // Proceed with the request
+  console.log('Request allowed!');
+} else {
+  // Deny the request
+  console.log('Access denied. Too many requests from this host.');
+}
+```
+
+## Support
+
+This library used jsdoc types and is tested in Chrome
+
--- index.js
+++ index.js
@@ -1,54 +1,91 @@
+/** Class representing a rate limiter for controlling access based on the number
+ * of requests within a specified time window for specific hosts.
+ */
 class HostLocker {
-
+    /**
+       * Creates an instance of HostLocker.
+       * @param {Object} config - Configuration options.
+       * @param {number} [config.maxCallThreshold=5] - Maximum allowed calls within the specified time window.
+       * @param {number} [config.secondsThreshold=5] - Time window in seconds.
+       * @param {string[]} [config.allowedHosts=[]] - An array of allowed hosts.
+       */
     constructor(config) {
-        const { maxCallThreshold=5, secondsThreshold=5, allowedHosts=[] } = config
-        this.hostCount = {};
-        this.maxCallThreshold = maxCallThreshold;
-        this.secondsThreshold = secondsThreshold;
-        this.allowedHosts = allowedHosts;
+        const { maxCallThreshold = 5, secondsThreshold = 5, allowedHosts = [] } = config
+        /**
+             * Object to store the request count for each host.
+             * @type {Object.<string, { c: number, d: Date }>}
+             */
+        this.hostCount = {}
+        /**
+             * Maximum allowed calls within the specified time window.
+             * @type {number}
+             */
+        this.maxCallThreshold = maxCallThreshold
+        /**
+             * Time window in seconds.
+             * @type {number}
+             */
+        this.secondsThreshold = secondsThreshold
+        /**
+             * An array of allowed hosts.
+             * @type {string[]}
+             */
+        this.allowedHosts = allowedHosts
     }
 
-    check = (hostname) => {
-        
-        if (!this.allowedHosts.includes(hostname)){
+    /**
+       * Checks if a given hostname is allowed to make a request based on the configured rate-limiting parameters.
+       * @param {string} hostname - The hostname to check.
+       * @returns {boolean} Returns true if the request is allowed, false otherwise.
+       */
+    check(hostname) {
+        if (!this.allowedHosts.includes(hostname)) {
             return false
         }
+
         let run = true
 
         if (!this.hostCount[hostname]) {
             this.newHostCount(hostname)
         } else {
             const mt = this.hostCount[hostname].d
-            const timestamp = new Date(mt);
+            const timestamp = new Date(mt)
 
-            const isOlder = this.timeStampOlder(timestamp);
+            const isOlder = this.timeStampOlder(timestamp)
             if (isOlder) {
-            this.newHostCount(hostname);
+                this.newHostCount(hostname)
             } else {
-                this.hostCount[hostname].c++;
+                this.hostCount[hostname].c++
             }
             if (this.hostCount[hostname].c > this.maxCallThreshold) {
                 run = false
             }
         }
         return run
-    };
+    }
 
+    /**
+       * Checks if a given timestamp is older than the specified time threshold.
+       * @param {Date} timestamp - The timestamp to check.
+       * @returns {boolean} Returns true if the timestamp is older, false otherwise.
+       */
+    timeStampOlder(timestamp) {
+        const timestampDate = new Date(timestamp)
+        const currentDate = new Date()
+        const timeDifferenceInSeconds = (currentDate - timestampDate) / 1000
+        return timeDifferenceInSeconds > this.secondsThreshold
+    }
 
-timeStampOlder = (timestamp) => {
-    const timestampDate = new Date(timestamp);
-    const currentDate = new Date();
-    const timeDifferenceInSeconds = (currentDate - timestampDate) / 1000;
-    return timeDifferenceInSeconds > this.secondsThreshold;
-}
-
-newHostCount = (hostname) => {
-    this.hostCount[hostname] = {
-        c: 1,
-        d: new Date()
+    /**
+       * Resets the request count for a given hostname.
+       * @param {string} hostname - The hostname to reset the count for.
+       */
+    newHostCount(hostname) {
+        this.hostCount[hostname] = {
+            c: 1,
+            d: new Date()
+        }
     }
 }
 
-}
-
-module.exports = HostLocker;
+module.exports = HostLocker
